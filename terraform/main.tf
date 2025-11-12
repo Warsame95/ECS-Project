d6595313d95840f633e0c3a6a3f39d31c8d3deff
Warsame <warsameahmed95@gmail.com>
2025-11-12 18:26:23 +0000

    Added dns module block and a few variables for other modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,18 @@ module "vpc" {
 module "ecs" {
     source = "./modules/ecs"
      vpc_id = var.vpc_id
+     alb_sg_id = module.alb.alb_sg_id
 }
 
 module "alb" {
   source = "./modules/alb"
   vpc_id = var.vpc_id
+  ecs_sg_id = module.ecs.ecs_sg_id
+  public_subnet_id = module.vpc.public_subnet_ids[0]
+}
+
+module "dns" {
+  source = "./modules/dns"
+  dns_name = module.alb.dns_name
+  zone_id = module.alb.zone_id
 }
