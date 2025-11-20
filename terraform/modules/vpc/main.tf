a2343d03318c2c430431b65789d2da9dfd0fff5a
Warsame <warsameahmed95@gmail.com>
2025-11-20 15:52:54 +0000

    Added nat gateway resource with eip and aws_route resources

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -73,8 +73,21 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public-rtb.id
 }
 
+// NAT gateway required also
 
+resource "aws_eip" "eip" {
+  domain = "vpc"
+}
 
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public[0].id
+}
 
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.private-rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
 
 
