8caa86e5159cee3fea46138c39ff90502bf816a2
Warsame <warsameahmed95@gmail.com>
2025-11-23 16:54:10 +0000

    Configured an s3 bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.19.0"
     }
   }
+  backend "s3" {
+    bucket = "warsame-memos-bucket"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {
