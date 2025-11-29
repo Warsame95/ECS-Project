5b7633f36632fac41d0a45fc93e138cd218f7c8f
Warsame <warsameahmed95@gmail.com>
2025-11-29 00:51:50 +0000

    Added policy to allow role to retrieve secrets

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -16,5 +16,31 @@ resource "aws_iam_role" "execution_role" {
 
 resource "aws_iam_role_policy_attachment" "execution_policy" {
   role = aws_iam_role.execution_role.name
-  policy_arn = var.policy_arn
+  policy_arn = var.execution_policy_arn
+}
+
+resource "aws_iam_policy" "ecs_execution_secrets" {
+  name = "${var.execution_role_name}-secrets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "secretsmanager:GetSecretValue",
+          "kms:Decrypt"
+        ]
+        Resource = [
+          var.my_secret_arn,
+          var.kms_key_arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "execution_secrets" {
+  role       = aws_iam_role.execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_secrets.arn
 }
--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -2,6 +2,14 @@ variable "execution_role_name" {
   type = string
 }
 
-variable "policy_arn" {
+variable "execution_policy_arn" {
+  type = string
+}
+
+variable "my_secret_arn" {
+  type = string
+}
+
+variable "kms_key_arn" {
   type = string
 }
