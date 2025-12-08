34757b6e8f1c4c534ced9d09591ef6132c39cc7b
Warsame <warsameahmed95@gmail.com>
2025-12-08 01:15:23 +0000

    Set skip_final_snapshot to true

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "db" {
   db_subnet_group_name = aws_db_subnet_group.private_subnet_group.name
   vpc_security_group_ids = [ aws_security_group.rds-sg.id ]
   storage_encrypted = true
+  skip_final_snapshot = true
 }
 
 resource "aws_db_subnet_group" "private_subnet_group" {
